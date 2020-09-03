b1864a7be89bc980bcf45eeaa56cfe24a1d4e84d
Ishika-07 <jjyotii82@gmail.com>
2020-09-03 17:11:26 +0530

    Addded Update Settings feature

--- components/MyHeader.js
+++ components/MyHeader.js
@@ -6,7 +6,7 @@ import { Header } from 'react-native-elements';
 const MyHeader =(props)=>{
     return(
         <Header 
-        centerComponent={{ text: props.title, style: { color: '#90A5A9', fontSize:20,fontWeight:"bold", } }}
+        centerComponent={{ text: props.title, style: { color: 'white', fontSize:20,fontWeight:"bold", } }}
         backgroundColor = "#ff5202"
         />
     )
--- screens/settingScreen.js
+++ screens/settingScreen.js
@@ -1,12 +1,147 @@
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, ScrollView, KeyboardAvoidingView} from 'react-native';
+import db from '../config';
+import firebase from 'firebase';
 
 export default class SettingScreen extends React.Component{
-    render(){
+
+    constructor(){
+        super()
+        this.state={
+          emailId : '',
+          firstName:'',
+          lastName:'',
+          address:'',
+          contact:'',
+          docId:''
+        }
+      }
+
+      getData=()=>{
+          var user = firebase.auth().currentUser;
+          var email = user.email;
+
+          db.collection('users').where('email_id','==', email).get().then((snapshot)=>{
+              snapshot.forEach((doc)=>{
+                  var data = doc.data()
+                  this.setState({
+                      emailId: data.email_id,
+                      firstName: data.first_name,
+                      lastName: data.last_name,
+                      address: data.address,
+                      contact: data.contact,
+                      docId:doc.id
+
+                  })
+                
+              })
+          })
+      }
+      updateData=()=>{
+        db.collection('users').doc(this.state.docId).update({
+            "first_name": this.state.firstName,
+            "last_name": this.state.lastName,
+            "address": this.state.address,
+            "contact": this.state.contact
+        })
+
+        alert('Profile Updated Successfully')
+      }
+
+      componentDidMount(){
+          this.getData()
+      }
+      
+      render(){
+
         return(
-            <View>
-                <Text>Settings</Text>
-            </View>
-        )
-    }
+           
+                <View>
+                    <ScrollView>
+                        <KeyboardAvoidingView>
+                            <Text>Registration</Text>
+
+                            <TextInput
+                            style={styles.formTextInput}
+                            placeholder ={"First Name"}
+                            maxLength ={15}
+                            onChangeText={(text)=>{
+                              this.setState({
+                                firstName: text
+                              })
+                            }}
+                            />
+
+                            <TextInput
+                            style={styles.formTextInput}
+                             placeholder ={"Last Name"}
+                             maxLength ={15}
+                             onChangeText={(text)=>{
+                               this.setState({
+                                 lastName: text
+                               })
+                             }}
+                            />
+
+                            <TextInput
+                            style={styles.formTextInput}
+                             placeholder ={"Contact"}
+                             maxLength ={10}
+                             keyboardType='numeric'
+                             onChangeText={(text)=>{
+                               this.setState({
+                                 contact: text
+                               })
+                             }}/>
+
+                            <TextInput
+                            style={styles.formTextInput}
+                             placeholder ={"Address"}
+                             multiline={true}
+                             onChangeText={(text)=>{
+                               this.setState({
+                                 address: text
+                               })
+                             }}/>
+                           <View>
+                            <TouchableOpacity
+                            style={styles.button}
+                                onPress={()=>{
+                                   this.updateData()
+                                
+                                }}>
+                                      <Text>Update</Text>
+                                </TouchableOpacity>
+                            </View>
+                        </KeyboardAvoidingView>
+                    </ScrollView>
+                </View>
+           
+        );
+      }
 }
+const styles = StyleSheet.create({
+    formTextInput:{
+        width:"75%",
+        height:35,
+        alignSelf:'center',
+        borderColor:'#ffab91',
+        borderRadius:10,
+        borderWidth:1,
+        marginTop:20,
+        padding:10
+      },
+      button:{
+        width:300,
+        height:50,
+        justifyContent:'center',
+        alignItems:'center',
+        borderRadius:25,
+        backgroundColor:"#FF5F49",
+        shadowColor: "#000",
+        shadowOffset: {
+           width: 0,
+           height: 8,
+        }
+    }
+})
